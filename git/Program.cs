a10906dbdd09e501879d2f3041e3d48c53a5e34f
Victor Portela <portelavictor@hotmail.com>
2021-07-28 11:03:32 -0300

    Acrescentada a mensagem desejando um bom dia

--- git/Program.cs
+++ git/Program.cs
@@ -5,7 +5,7 @@ namespace git {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
-            Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia");
 
             Console.ReadLine();
         }
